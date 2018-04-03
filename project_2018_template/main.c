d63c40a2c23c56f2f9642446ab1a0b647a3d4f6b
francois1997 <37047753+francois1997@users.noreply.github.com>
2018-04-03 22:57:31 +0200

    completion of the buffer problem for creationthreads + commentary on how function works

--- project_2018_template/main.c
+++ project_2018_template/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <pthread.h>
 
 #include <sys/types.h>
@@ -8,6 +9,22 @@
 #include <fcntl.h>
 
 #include "fractal.h"
+
+
+
+/* it is important to implement a rendez vous semaphore in main for it to wait for the threads to finish before 
+ * finishing itself
+ *
+ *
+ *
+ *
+*/
+
+
+pthread_mutex_t buffycreate;
+sem_t full;
+sem_t empty;
+
 
 
 struct parametres {
@@ -35,8 +52,25 @@ void *FractCreate (void *param)
 	
 	int test = 1;
 	
+	/*  This section is about reading the files containing the frctal structure
+	 *	The first part is about getting the name of the fractal
+	 *	the second section is about the other data like width height a and b
+	 *	the third section is about putting the pointer of the fractal in the buffer
+	 *	
+	*/
+	
 	while (test == 1)
 	{
+		/* this section primarily works on the name of the fractal,  
+		 * first it check if we are not a the end of the file or that an error occurs
+		 * then it check if it is not an empty line or a comment
+		 * it then store each character until the first space character
+		 * 
+		 *
+		*/
+		
+		
+		
 		int i = 0;
 		char ispace = '';
 		while(ispace != ' ' && i < 66)
@@ -69,11 +103,19 @@ void *FractCreate (void *param)
 				*(name + i) = '\0';
 			
 			i++;
+			if(i == 66)
+				i = 100;
 		}
+		
+		/* this condition here is to skip empty line and comment
+		 * when we enter the conditions we get the static information of the fractal
+		 * The reading is a dangerous process, if a single line doesn't respect the syntax, the threads terminate
+		 * and the rest of the file is skipped
+		*/
 		
 		if(i != 100)
 		{
-			if (read(fd, (void*)width, 4) < 4)
+			if (read(fd, (void*)(&width), 4) < 4)
 			{
 				close(para->fd);
 				free(name);
@@ -81,7 +123,7 @@ void *FractCreate (void *param)
 				pthread_exit(NULL);
 			}
 			lseek(fd, 1, SEEK_CUR);
-			if (read(fd, (void*)height, 4) < 4)
+			if (read(fd, (void*)(&height), 4) < 4)
 			{
 				close(para->fd);
 				free(name);
@@ -89,7 +131,7 @@ void *FractCreate (void *param)
 				pthread_exit(NULL);
 			}
 			lseek(fd, 1, SEEK_CUR);
-			if (read(fd, (void*)a, sizeof(double)) < sizeof(double))
+			if (read(fd, (void*)(&a), sizeof(double)) < sizeof(double))
 			{
 				close(para->fd);
 				free(name);
@@ -97,7 +139,7 @@ void *FractCreate (void *param)
 				pthread_exit(NULL);
 			}
 			lseek(fd, 1, SEEK_CUR);
-			if (read(fd, (void*)b, sizeof(double)) < sizeof(double))
+			if (read(fd, (void*)(&b), sizeof(double)) < sizeof(double))
 			{
 				close(para->fd);
 				free(name);
@@ -105,14 +147,29 @@ void *FractCreate (void *param)
 				pthread_exit(NULL);
 			}
 		}
+		
+		/* This loop serve to go to the next line, only works with file who use LF to go to next the line
+		 * It has a second purpose and it is to get out of the greater loop when we are a the end of the file
+		 * 
+		*/
 		
 		while (ispace != '\n' && test == 1)
 			test = read(fd, (void*)ispace, 1);
 		
+		struct fractal *fracty = fractal_new(name, width, height,  a,  b);
+		if (fracty != NULL)
+		{
+			sem_wait(&empty);
+			pthread_mutex_lock(&buffycreate);
+			*((para->buffer) + sem_getvalue(&full, 0)) = fracty;
+			pthread_mutex_unlock(&buffycreate);
+			sem_post(&full);
+			//producer comsumer problem
+		}
+		else if (fracty == NULL)
+			fprintf(stderr, "non-critical Malloc Error\n");
 		
 		
-		
-		//producer comsumer problem
 	}
 	
 	
@@ -154,6 +211,13 @@ void RthreadCreation(int readingThreads, char *filename[], pthread_t threadlist[
 		pthread_create(&(threadlist[i]), NULL, &FractCreate, (void *)(argum + i));
 	}
 }
+ */
+ 
+ /* this function try to open all the file that have been passed in the arguments
+  * it checks that it is not the - option and then try to open the file, in case of failure
+  * it display an error on stderr but still continue to open the others files, when - is detected
+  * the stdin is open for the users to input and control is set to 1 to avoid trying to open more than once the stdin
+  * it is important to note that all the other - after the first will be ignored
  */
  
  
@@ -218,6 +282,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "malloc fail");
 		return -1;
 	}
+	int sem_init(&empty, 0, argc*2);
+	int sem_init(&full, 0, 0);
 	
 	
     if (argc => 3)
