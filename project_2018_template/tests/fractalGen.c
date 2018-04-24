b10052ba852d978d4ebdbbb906bd84a132242afd
francois <37047753+francois1997@users.noreply.github.com>
2018-04-24 21:59:32 +0200

    small fixes

--- project_2018_template/tests/fractalGen.c
+++ project_2018_template/tests/fractalGen.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /*	Create int fractals pattern in each filename
  *	each fractal has a different name and complex number
