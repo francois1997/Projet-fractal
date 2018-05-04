4af544ac9004d6f38652f64f64ef1cf280bf2b66
francois1997 <37047753+francois1997@users.noreply.github.com>
2018-05-04 18:36:30 +0200

    fixed the libfractal directory weirdness

--- project_2018_template/libfractal/fractal.c
+++ project_2018_template/libfractal/fractal.c
@@ -1,44 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fractal.h"
-#include <stdint.h>
 #include <string.h>
+#include "fractal.h"
 
 struct fractal *fractal_new(const char *name, int width, int height, double a, double b)
 {
-  struct fractal *newFract = (struct fractal*)malloc(sizeof(struct fractal)); //allocate memory for fractal structure
+    struct fractal *newFract = (struct fractal*)malloc(sizeof(struct fractal)); //allocate memory for fractal structure
 	if (newFract == NULL)
+	{
+		fprintf(stderr, "Fractal malloc error\n");
 		return NULL;
-
+	}
+	
 	newFract->name = (char *)malloc(strlen(name)+1);							//allocate memory for fractal name
 	if (newFract->name == NULL)
 	{
 		free(newFract);
+		fprintf(stderr, "name Fractal malloc error\n");
 		return NULL;
 	}
 	strcpy(newFract->name, name);
-
-	newFract->image = (uint16_t *)malloc(sizeof(uint16_t)*height*width);		//allocate memory for the whole image
-	if ((newFract->image) == NULL)
+	
+	/* newFract->image = (uint16_t **)malloc(sizeof(uint16_t *)*height);		//allocate memory for the table of pointer
+	if(newFract->image == NULL)
+	{
+		free(newFract->name);
+		free(newFract);
+		return NULL;
+	}
+	
+	*(newFract->image) = (uint16_t *)malloc(sizeof(uint16_t)*height*width);		//allocate memory for the whole image
+	if (*(newFract->image) == NULL)
 	{
 		free(newFract->image);
 		free(newFract->name);
 		free(newFract);
 		return NULL;
 	}
-
-
+	for (int i=1;i < height; i++)												//place the pointer of the first table towards their respective line
+	{
+		*((newFract->image)+i) = *(newFract->image)+i*width;
+	} */
+	
+	newFract->image = (uint16_t *)malloc(sizeof(uint16_t)*height*width);		//allocate memory for the whole image
+	if ((newFract->image) == NULL)
+	{
+		free(newFract->image);
+		free(newFract->name);
+		free(newFract);
+		fprintf(stderr, "image Fractal malloc error\n");
+		return NULL;
+	}
+	
+	
 	newFract->width = width;
 	newFract->height = height;
 	newFract->a = a;
 	newFract->b = b;
-
+	
     return newFract;
 }
 
 void fractal_free(struct fractal *f)
 {
-  free(f->name);
+    free((f->name));
 	free(f->image);
 	free(f);
 }
@@ -52,8 +77,8 @@ int fractal_get_value(const struct fractal *f, int x, int y)
 {
     if(x > f->width || y > f->height)
 		return -1;
-
-
+	
+	
     return *((f->image) + x + (f->width)*y);
 }
 
@@ -61,7 +86,7 @@ void fractal_set_value(struct fractal *f, int x, int y, int val)
 {
     if(x > f->width || y > f->height)
 	{
-		printf("SEG_fault : set_value");
+		printf("SEG_fault : set_value\n");
 		exit(-1);
 	}
 	*((f->image) + x + (f->width)*y) = val;
@@ -87,17 +112,22 @@ double fractal_get_b(const struct fractal *f)
     return f->b;
 }
 
-size_t strlen(const char *string)
+
+int argCmp(char *string1, char *string2)
 {
-	if (string == NULL)
-		return -1;
-
 	int i =0;
-	while (*(string + i) != '\0' && (i <= 65))
+	while (*(string1 + i) != '\0' && *(string2 +i) != '\0')
+	{
+		if (*(string1 + i) != *(string2 + i))
+			return 0;
+		
 		i++;
-
-	if (i == 66)
-		return -1;
-
-	return (size_t)i;
+	}
+	if(*(string1 + i) == *(string2 + i))
+		return 1;
+	
+	return 0;
 }
+
+
+
--- project_2018_template/libfractal/fractal.h
+++ project_2018_template/libfractal/fractal.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #ifndef _FRACTAL_H
 #define _FRACTAL_H
 
@@ -7,6 +9,7 @@ struct fractal {
 	unsigned long height;
 	double a;
 	double b;
+	double moyenne;
 	uint16_t *image;
 	};
 
@@ -115,25 +118,16 @@ int fractal_compute_value(struct fractal *f, int x, int y);
 int write_bitmap_sdl(const struct fractal *f, const char *fname);
 
 /*
- * strlen
+ * argCmp
  *
- * compte le nombre de characters dans un tableau de characters
+ * Compare two string despite being called argCmp
  *
- * @string : tableau de characters
- * @return : taille du tableau si pas d'erreurs, -1 sinon
-*/
-size_t strlen(const char *string);
+ * @string1 first string to compare
+ * @string2 second string to compare
+ * @return 1 if the strings matches, 0 otherwise
+ * note : the \0 character has to match as well
+ */
 
-/*
- * stpcpy
- *
- * copy un tableau de characters vers une adresse mémoire donnée
- *
- * @dest : destination ou le tableau sera copié, doit avoir la bonne taille
- * @src : tableau de characters à copié
- *
- * @return : ne retourne rien
-*/
-void strcpy(char *dest, const char *src)
+int argCmp(char *string1, char *string2);
 
 #endif
